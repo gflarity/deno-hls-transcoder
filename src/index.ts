03b8aface32c219f473f3f4322bd1d6dfd8b854c
Adam Katora <adamkatora@gmail.com>
2022-05-16 19:45:25 -0400

    Make constructor options parameter optional

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { HLSTranscoderOptions } from "./types"
+import { HLSTranscoderOptions } from './types'
 import { spawn } from 'child_process'
-import DefaultRenditions from "./default-renditions"
+import DefaultRenditions from './default-renditions'
 import fs from 'fs'
 
 export default class Transcoder {
@@ -9,33 +9,36 @@ export default class Transcoder {
   options: HLSTranscoderOptions
 
   constructor(
-    inputPath: string, 
-    outputPath: string, 
-    options: HLSTranscoderOptions) {
-      this.inputPath = inputPath
-      this.outputPath = outputPath
-      this.options = options || {}
+    inputPath: string,
+    outputPath: string,
+    options: HLSTranscoderOptions = {}
+  ) {
+    this.inputPath = inputPath
+    this.outputPath = outputPath
+    this.options = options
   }
 
   public async transcode() {
     let commands: Array<string>
     try {
       commands = await this.buildCommands()
-    } catch(err) {
+    } catch (err) {
       return err
     }
 
     let masterPlaylist: any
     try {
       masterPlaylist = await this.writePlaylist()
-    } catch(err) {
+    } catch (err) {
       return err
     }
 
-    const showLogs = this.options.showLogs ? this.options.showLogs : false;
+    const showLogs = this.options.showLogs ? this.options.showLogs : false
 
     return new Promise((resolve, reject) => {
-      const ffmpeg = this.options.ffmpegPath ? spawn(this.options.ffmpegPath, commands) : spawn('ffmpeg', commands);
+      const ffmpeg = this.options.ffmpegPath
+        ? spawn(this.options.ffmpegPath, commands)
+        : spawn('ffmpeg', commands)
 
       ffmpeg.stdout.on('data', (data: any) => {
         if (showLogs) {
@@ -59,37 +62,70 @@ export default class Transcoder {
   }
 
   private buildCommands(): Promise<string[]> {
-    return new Promise((resolve)  => {
+    return new Promise((resolve) => {
       let commands: Array<string> = ['-hide_banner', '-y', '-i', this.inputPath]
       const renditions = this.options.renditions || DefaultRenditions
 
       for (let i = 0, len = renditions.length; i < len; i++) {
-        const r = renditions[i];
-        commands = commands.concat(['-vf', `scale=w=${r.width}:h=${r.height}:force_original_aspect_ratio=decrease`, '-c:a', 'aac', '-ar', '48000', '-c:v', 'h264', `-profile:v`, r.profile, '-crf', '20', '-sc_threshold', '0', '-g', '48', '-hls_time', r.hlsTime.toString(), '-hls_playlist_type', 'vod', '-b:v', r.bv, '-maxrate', r.maxrate, '-bufsize', r.bufsize, '-b:a', r.ba, '-hls_segment_filename', `${this.outputPath}/${r.ts_title}_%03d.ts`, `${this.outputPath}/${r.height}.m3u8`])
+        const r = renditions[i]
+        commands = commands.concat([
+          '-vf',
+          `scale=w=${r.width}:h=${r.height}:force_original_aspect_ratio=decrease`,
+          '-c:a',
+          'aac',
+          '-ar',
+          '48000',
+          '-c:v',
+          'h264',
+          `-profile:v`,
+          r.profile,
+          '-crf',
+          '20',
+          '-sc_threshold',
+          '0',
+          '-g',
+          '48',
+          '-hls_time',
+          r.hlsTime.toString(),
+          '-hls_playlist_type',
+          'vod',
+          '-b:v',
+          r.bv,
+          '-maxrate',
+          r.maxrate,
+          '-bufsize',
+          r.bufsize,
+          '-b:a',
+          r.ba,
+          '-hls_segment_filename',
+          `${this.outputPath}/${r.ts_title}_%03d.ts`,
+          `${this.outputPath}/${r.height}.m3u8`
+        ])
       }
 
-      resolve(commands);
+      resolve(commands)
     })
   }
 
   private writePlaylist() {
     return new Promise((resolve) => {
-      let m3u8Playlist = 
-      `#EXTM3U
+      let m3u8Playlist = `#EXTM3U
       #EXT-X-VERSION:3`
-    
+
       const renditions = this.options.renditions || DefaultRenditions
-              
-      for (let i = 0, len = renditions.length; i < len; i++){
-          const r = renditions[i];
-          m3u8Playlist += 
-          `#EXT-X-STREAM-INF:BANDWIDTH=${r.bv.replace('k', '000')},RESOLUTION=${r.width}x${r.height}${r.height}.m3u8`
+
+      for (let i = 0, len = renditions.length; i < len; i++) {
+        const r = renditions[i]
+        m3u8Playlist += `#EXT-X-STREAM-INF:BANDWIDTH=${r.bv.replace(
+          'k',
+          '000'
+        )},RESOLUTION=${r.width}x${r.height}${r.height}.m3u8`
       }
-      
+
       const m3u8Path = `${this.outputPath}/index.m3u8`
-      fs.writeFileSync(m3u8Path, m3u8Playlist);
-      
-      resolve(m3u8Path);
+      fs.writeFileSync(m3u8Path, m3u8Playlist)
+
+      resolve(m3u8Path)
     })
   }
 }
