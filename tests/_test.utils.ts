5502f60b909d8b0f71da4cb131c4bd07130ecb1b
Adam Katora <adamkatora@gmail.com>
2022-05-17 17:23:39 -0400

    Create initial tests files

---
+++ tests/_test.utils.ts
@@ -0,0 +1,22 @@
+import * as fs from 'fs'
+import * as path from 'path'
+
+export async function clearOutputFolder(): Promise<void> {
+  // Currently assuming all transcoding is going into output/ dir
+  const directory = `${__dirname}/output`
+  await fs.readdir(directory, (err, files) => {
+    if(err && err.code === 'ENOENT') {
+      // No dir to empty, just return
+      return
+    }
+    if (err) throw err;
+
+    for(const file of files) {
+      fs.unlink(path.join(directory, file), err => {
+        if (err) throw err;
+      })
+    }
+  })
+
+  return
+}
